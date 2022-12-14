bfa716cc5b0e119a5f045f9f4fb74a30fcb8de06
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2022-12-14 17:42:21 +0530

    server side completed successfully

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ app.use(express.json())
 // routes
 
 const authRoute=require('./routes/auth')
-
+const userRoute=require('./routes/user');
+const postRoute=require('./routes/post');
+const categoryRoute=require('./routes/categories');
+const multer=require("Multer")
 
 // DB connection
 mongoose.connect(process.env.MONGO_URL)
@@ -18,12 +21,38 @@ mongoose.connect(process.env.MONGO_URL)
     console.log("DB Connection Error",err);
 });
 
+
+// Multer implementation
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+       cb(null,'images') 
+    },filename:(req,file,cb)=>{
+        cb(null,"req.body.name")
+    }
+})
+
+const upload=multer({storage:storage});
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    res.status(200).json("File has been uploaded")
+});
+
 // use routes
+// this is for auth
 app.use('/api/auth',authRoute);
 
+// this is for user
+app.use('/api/user',userRoute);
 
+// this is for post
+app.use('/api/posts',postRoute);
+
+// this is for category
+app.use('/api/categories',categoryRoute);
+
+// Multer imple,ment
 
 // app listen 
+
 app.listen("8000",(err)=>{
     if(err){
         console.log("Error In starting Server....");
--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,77 @@
 const express=require('express')
 const router=express.Router();
 const User=require('../models/User');
+const Post=require('../models/Post');
+const bcrypt=require('bcrypt');
 
 
 // UPDATE
-router.post("/register", async(req,res)=>{
-    try{
-        
-    }catch(err){
-        res.status(500).json(err);
+router.put("/:id", async(req,res)=>{
+    // here we are auth user with passing the userId in params and bcrypt it 
+        if(req.body.userId=== req.params.id){
+            if(req.body.password){
+                const salt=await bcrypt.genSalt(10);
+                req.body.password=await bcrypt.hash(req.body.password,salt);
+            }
+        try{
+            const updatedUser=await User.findByIdAndUpdate(req.params.id,{
+                $set:req.body,
+            },
+            // this will help us to update the db and send the updated data
+            {new:true}
+            );
+            res.status(200).json(updatedUser);
+            
+        }catch(err){
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(401).json("You Can update only your account !");
+
     }
 });
 
 
 //DELETE
 
+router.delete("/:id", async(req,res)=>{
+    // here we are auth user with passing the userId in params and bcrypt it 
+        if(req.body.userId=== req.params.id){
+            // here we need to find the user and then assosiated post then delete the post after that we can delete the user 
+            try{
+                const user=await User.findById(req.params.id)
+            
+                try{
+                    // if the user.username and username is same then delete those post posted by user
+                    await Post.deleteMany({username:user.username});
+                    await User.findByIdAndDelete(req.params.id)
+                    
+                    res.status(200).json("User deleted...");
+                    
+                }catch(err){
+                    res.status(500).json(err);
+                }
+            }catch(err){
+                res.status(404).json("User not found");
+                
+            }
+        }else{
+            res.status(401).json("You Can Delete only your account !");
 
+        }
+    });
+
+// GET One User
+
+router.get("/:id", async(req,res)=>{
+    try{
+        const user=await User.findById(req.params.id);
+        const {password,...others}=user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 
 
