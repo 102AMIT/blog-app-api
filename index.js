b102f698e86a47104006a01f02df544d9ff6b2ea
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2022-12-14 06:30:10 +0530

    model creation is completed

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express=require("express");
 const app=express();
+const mongoose=require("mongoose");
+const dotenv=require("dotenv").config();
+
+// DB connection
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("Connected to mongoDB"))
+.catch(err=>{
+    console.log("DB Connection Error",err);
+});
+
 
 app.use('/',(req,res)=>{
     res.end(`<h1>Hello</h1>`)
